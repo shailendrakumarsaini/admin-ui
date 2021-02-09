7bbc1363b62a1802d86d83545b1a66949c75e871
shailendra kumar <sainishailendra1996@gmail.com>
2021-02-09 15:55:43 +0530

    components created

--- src/app/components/dashboard/dashboard-routing.module.ts
+++ src/app/components/dashboard/dashboard-routing.module.ts
@@ -3,11 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
 import { UserComponent } from './user/user.component';
+import { CategoryComponent } from './category/category.component';
+import { CourseComponent } from './course/course.component';
+import { AssignmentComponent } from './assignment/assignment.component';
+import { QuestionComponent } from './question/question.component';
 
 const routes: Routes = [
     { path: '', component: DashboardComponent, 
       children : [
         { path: 'user', component: UserComponent },
+        { path: 'category', component: CategoryComponent },
+        { path: 'course', component: CourseComponent },
+        { path: 'assignment', component: AssignmentComponent },
+        { path: 'question', component: QuestionComponent },
         { path: '**', redirectTo:'user', pathMatch:'full' }
       ]
     },
--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -7,6 +7,10 @@ import { NavbarComponent } from './../../shared/navbar/navbar.component';
 import { SidePanelComponent } from './../../shared/side-panel/side-panel.component';
 import { FooterComponent } from './../../shared/footer/footer.component';
 import { UserComponent } from './user/user.component';
+import { CategoryComponent } from './category/category.component';
+import { CourseComponent } from './course/course.component';
+import { AssignmentComponent } from './assignment/assignment.component';
+import { QuestionComponent } from './question/question.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { UserComponent } from './user/user.component';
     NavbarComponent,
     SidePanelComponent,
     FooterComponent,
-    UserComponent
+    UserComponent,
+    CategoryComponent,
+    CourseComponent,
+    AssignmentComponent,
+    QuestionComponent
   ],
   imports: [
     CommonModule,
