e10b98c60845d4eeab2d428335cfc34fe7f8a1a5
shailendra kumar <sainishailendra1996@gmail.com>
2021-02-09 16:28:20 +0530

    added api call to user component

--- src/app/components/dashboard/user/user.component.ts
+++ src/app/components/dashboard/user/user.component.ts
@@ -6,15 +6,29 @@ import { ApiService } from './../../../services/api.service';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-
+  usersList:any = [];
+  fieldName : string;
+  order : boolean = false;
   constructor(
     private apiService :ApiService
   ) { }
 
-  ngOnInit(): void {
+  async ngOnInit(){
+    await this.getUsersList();
+  }
+
+  getUsersList(){
     this.apiService.get('user').subscribe(res=>{
       console.log(res);
+      this.usersList = res;
     }),err=>{ console.log(err);};
+  }
+
+  orderByFunction(value){
+    if (this.fieldName === value) {
+      this.order = !this.order;
+    }
+    this.fieldName = value;
   }
 
 }
