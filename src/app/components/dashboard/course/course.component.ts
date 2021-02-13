96e32e55b75ef11cb6bb6cd16ca8b3a2942dec08
shailendra kumar <sainishailendra1996@gmail.com>
2021-02-13 13:57:07 +0530

    added api

--- src/app/components/dashboard/course/course.component.ts
+++ src/app/components/dashboard/course/course.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-course',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private apiService: ApiService
+  ) { }
 
   ngOnInit(): void {
+    this.apiService.get('dashboard').subscribe(
+      res => {
+        console.log('[response]',res);
+      },
+      err => console.error(err)
+    );
   }
 
 }
