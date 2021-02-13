9147c7869c07bf1796ba19483f100df51402d3eb
shailendra kumar <sainishailendra1996@gmail.com>
2021-02-13 15:55:45 +0530

    added toaster in interceptor

--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@angular/core";
-import { HttpInterceptor, HttpRequest, HttpXsrfTokenExtractor, HttpHandler, HttpEvent } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { ApiService } from './api.service';
 import { Observable } from "rxjs/internal/Observable";
 import { finalize } from 'rxjs/operators';
-
+import { tap } from "rxjs/operators";
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 @Injectable({
     providedIn:'root'
 })
@@ -11,7 +13,8 @@ export class Interceptor implements HttpInterceptor {
     private requests: HttpRequest<any>[] = [];
     constructor(
         private apiService: ApiService,
-        private tokenExtractor: HttpXsrfTokenExtractor,
+        private router: Router,
+        private toastr : ToastrService
     ) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -30,7 +33,30 @@ export class Interceptor implements HttpInterceptor {
 
 
         return next.handle(req).pipe(
-
+            // <<<<<<<<<<<<<<<< Authorization >>>>>>>>>>>>>>>>>
+            tap(
+                event => {
+                    if (event instanceof HttpResponse) {
+                        if (event.status == 401) {
+                            // console.log(event.body.message);
+                            this.toastr.error('Session Expired');
+                            sessionStorage.clear();
+                            this.router.navigate(['/auth/login']);
+                        }
+                    }
+                },
+                error => {
+                    if (error instanceof HttpErrorResponse) {
+                        if (error.status == 401) {
+                            // console.log(error);
+                            this.toastr.error('Session Expired');
+                            sessionStorage.clear();
+                            this.router.navigate(['/auth/login']);
+                        }
+                    }
+                }
+            ),
+            // <<<<<<<<<<<<<<<< Authorization >>>>>>>>>>>>>>>>>
             finalize(() => {
                 this.apiService.isLoading.next(false);
             }
